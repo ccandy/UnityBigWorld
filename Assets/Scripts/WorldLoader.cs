232eb0d1010cca133e1136641679a403c93e264f
ccandy <ccunandy2001@gmail.com>
2021-01-28 21:13:18 -0800

    add unity project

---
+++ Assets/Scripts/WorldLoader.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldLoader : MonoBehaviour
+{
+
+
+    public Transform PlayerTransformer;
+    public float CheckTimer = 1;
+    void Start()
+    {
+        StartCoroutine(PlayerPositionChecker());
+    }
+
+    IEnumerator PlayerPositionChecker()
+    {
+        while (true)
+        {
+            if(PlayerTransformer != null)
+            {
+                Debug.Log("Player pos " + PlayerTransformer.position);
+            }
+            else
+            {
+                Debug.LogError("Player is null");
+            }
+            yield return new WaitForSeconds(CheckTimer);
+        }
+        
+
+    } 
+    
+}
